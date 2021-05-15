8c20c774058b4b5e233b9a50f8041d249cab28cb
Joaquin <jogaraba@icloud.com>
2021-05-15 20:06:47 +0200

    apis

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,5 +1,13 @@
 // Geonames API
+const geonamesUrl = 'http://api.geonames.org/searchJSON?q=';
+const geonamesUrlTwo = '&maxRows=1&username=';
+const geonamesKey = 'jogaraba';
 
-// WeatherBit API
+// Weatherbit API
+const weatherbitUrl = 'http://api.weatherbit.io/v2.0/forecast/daily?';
+const WeatherbitKey = 'f2c0becdfbbc4697987d77c9278d4151';
 
 // Pixabay API
+const pixabayUrl = 'https://pixabay.com/api/?key=';
+const pixabayKey = '21600245-8c008566b7aa3e4a91c9543ab';
+
